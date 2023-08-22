e2f5e1db67ee6c3fe76c1f95bb7b841c37c9c085
liisnormann <liis.normann@gmail.com>
2023-08-22 19:25:01 +0300

    Navigation between screens and tabs

--- src/components/Checkbox/styles.js
+++ src/components/Checkbox/styles.js
@@ -1,33 +1,25 @@
-import { StyleSheet } from "react-native";
-import { colors } from "../../utils/colors";
+import {StyleSheet} from 'react-native';
+import {colors} from '../../utils/colors';
 
-export const styles = StyleSheet.create ({
-
-    container: {
-     borderColor: colors.grey,
-     borderWidth: 1,
-     borderRadius: 4,
-     width: 22,
-     height: 22,
-     marginLeft: 20,
+export const styles = StyleSheet.create({
+  container: {
+    borderColor: colors.grey,
+    borderWidth: 1,
+    borderRadius: 4,
+    width: 22,
+    height: 22,
+    marginLeft: 20,
     // marginRight: 20,
-   
-    },
-    innerContainer: {
-        backgroundColor: colors.grey,
-        width: '100%',
-        height:'100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        
-       
-    },
-    checkIcon: {
-       width: 12,
-       height: 9,
-       
-    },
-   
-
-    
-})
+  },
+  innerContainer: {
+    backgroundColor: colors.grey,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  checkIcon: {
+    width: 12,
+    height: 9,
+  },
+});
