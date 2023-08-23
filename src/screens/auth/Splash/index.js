5fb580120c124c8a6a96b725deab882abbf192f8
liisnormann <liis.normann@gmail.com>
2023-08-23 11:35:45 +0300

    create homepage screen

--- src/screens/auth/Splash/index.js
+++ src/screens/auth/Splash/index.js
@@ -1,40 +1,36 @@
-import React from 'react';
-import {Text, View, Image, Pressable} from 'react-native';
+import React from "react";
+import {Text, View, Image, Pressable} from "react-native";
 // import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
-import Button from '../../../components/Button';
-import {styles} from './styles';
+import Button from "../../../components/Button";
+import {styles} from "./styles";
 
 const Splash = ({navigation}) => {
-  console.log('navigation => ', navigation);
+    console.log('navigation => ', navigation)
 
-  const onSignup = () => {
-    navigation.navigate('Signup');
-  };
+    const onSignup = () => {
+        navigation.navigate('Signup')
+    }
 
-  const onSignin = () => {
-    navigation.navigate('Signin');
-  };
+    const onSignin = () => {
+        navigation.navigate('Signin')
+    }
 
-  return (
-    <View style={styles.container}>
-      <Image
-        resizeMode="contain"
-        style={styles.image}
-        source={require('../../../assets/splash_image.png')}
-      />
+    return (
+        <View style={styles.container}>
+            <Image resizeMode="contain" style={styles.image} source={require('../../../assets/splash_image.png')}/>
+            
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>You'll Find</Text>
+                <Text style={[styles.title, styles.innerTitle]}>All you need </Text>
+                <Text style={styles.title}>Here!</Text>
+            </View>
 
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>You'll Find</Text>
-        <Text style={[styles.title, styles.innerTitle]}>All you need </Text>
-        <Text style={styles.title}>Here!</Text>
-      </View>
+            <Button onPress={onSignup} title="Sign Up" />
 
-      <Button onPress={onSignup} title="Sign Up" />
-
-      <Pressable onPress={onSignin} hitSlop={20}>
-        <Text style={styles.footerText}>Sign In</Text>
-      </Pressable>
-    </View>
-  );
-};
-export default Splash;
+            <Pressable onPress={onSignin} hitSlop={20}>
+                <Text style={styles.footerText}>Sign In</Text>
+            </Pressable>
+        </View>
+    )
+}
+export default Splash
